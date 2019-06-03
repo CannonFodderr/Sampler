b6c947a0a52ed71d544654ab614e8d2dd9bb6765
Idan Izhaki <izhaki.idan@gmail.com>
2019-06-03 10:32:47 +0300

    Update Manifest

--- public/sw.js
+++ public/sw.js
@@ -1,33 +1,65 @@
+let CACHE_NAME = 'samplerv1';
+let urlsToCache = [
+    '/',
+];
 if('serviceWorker' in navigator){
-    console.log("SW Available")
     navigator.serviceWorker.register('./sw.js')
     .then(reg => {
-        console.log("SW Registered");
+        reg.addEventListener('updatefound', () => {
+            let newWorker = reg.installing
+            let accepted = confirm("Install new update")
+            if(accepted){
+                newWorker.skipWaiting()
+            }
+        })
     })
     .catch(err => console.error(err))
 }
+self.addEventListener('updatefound', () => {
+    console.log("Found new update")
+})
 
-self.addEventListener('fetch', e => {
-    e.preventDefault()
-    e.respondWith(
-        caches.match(e.request)
-        .then(response => {
-            if(response) {
+self.addEventListener('install', function(event) {
+  // Perform install steps
+event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
                 return response;
             }
-            return fetch(e.request)
-            .then(response => {
-                var responseToCache = response.clone();
-                caches.open('samplerv1')
-                    .then(function(cache) {
-                        cache.put(e.request, responseToCache);
-                        return response;
-                });
-            })
             
-        })
-        
-
-    )
-    
-})
+            return fetch(event.request).then(
+                function(response) {
+                    // Check if we received a valid response
+                    if(!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+                    
+                    // IMPORTANT: Clone the response. A response is a stream
+                    // and because we want the browser to consume the response
+                    // as well as the cache consuming the response, we need
+                    // to clone it so we have two streams.
+                    var responseToCache = response.clone();
+                    
+                    caches.open(CACHE_NAME)
+                    .then(function(cache) {
+                        cache.put(event.request, responseToCache);
+                    });
+                    
+                    return response;
+                }
+                );
+            })
+            );
+        });
